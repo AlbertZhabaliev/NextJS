053e6562622e6c5d895fe40a28c8c9115c7b09af
metamarphosa <a_z@tuta.io>
2021-05-15 12:51:41 +0200

    test commit

---
+++ dj-events-frontend/pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+    return (
+        <div>
+            <h1>SlugEvents </h1>
+        </div>
+    )
+}
---
+++ dj-events-frontend/pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPaje() {
+    return (
+        <div>
+            <h1>My Eevents</h1>
+        </div>
+    )
+}
